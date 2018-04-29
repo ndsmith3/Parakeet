02d49302996089c83dbcd2a6d73c0887077f4835
Nick Smith <nsmith458914@gmail.com>
2018-04-29 14:50:41 -0400

    Add Char to numeric types.

--- src/main/scala/com/ndsmith3/parakeet/ast/Numeric.scala
+++ src/main/scala/com/ndsmith3/parakeet/ast/Numeric.scala
@@ -14,64 +14,110 @@ trait Numeric extends Primitive {
 
 case class Integer(value: Int) extends Numeric {
   override def +(that: Numeric): Numeric = that match {
-    case Integer(thatValue) => Integer(value + thatValue)
-    case Float(thatValue)   => Float(value + thatValue)
+    case Integer(thatValue)   => Integer(value + thatValue)
+    case Float(thatValue)     => Float(value + thatValue)
+    case Character(thatValue) => Integer(value + thatValue)
   }
 
   override def -(that: Numeric): Numeric = that match {
-    case Integer(thatValue) => Integer(value - thatValue)
-    case Float(thatValue)   => Float(value - thatValue)
+    case Integer(thatValue)   => Integer(value - thatValue)
+    case Float(thatValue)     => Float(value - thatValue)
+    case Character(thatValue) => Integer(value - thatValue)
   }
 
   override def *(that: Numeric): Numeric = that match {
-    case Integer(thatValue) => Integer(value * thatValue)
-    case Float(thatValue)   => Float(value * thatValue)
+    case Integer(thatValue)   => Integer(value * thatValue)
+    case Float(thatValue)     => Float(value * thatValue)
+    case Character(thatValue) => Integer(value * thatValue)
   }
 
   override def /(that: Numeric): Numeric = that match {
-    case Integer(thatValue) => Integer(value / thatValue)
-    case Float(thatValue)   => Float(value / thatValue)
+    case Integer(thatValue)   => Integer(value / thatValue)
+    case Float(thatValue)     => Float(value / thatValue)
+    case Character(thatValue) => Integer(value / thatValue)
   }
 
   override def %(that: Numeric): Numeric = that match {
-    case Integer(thatValue) => Integer(value % thatValue)
-    case Float(thatValue)   => Float(value   % thatValue)
+    case Integer(thatValue)   => Integer(value % thatValue)
+    case Float(thatValue)     => Float(value   % thatValue)
+    case Character(thatValue) => Integer(value % thatValue)
   }
 
   override def ^(that: Numeric): Numeric = that match {
     case Integer(thatValue) => Integer(Math.pow(value, thatValue).toInt)
     case Float(thatValue)   => Float(Math.pow(value, thatValue))
+    case Character(_)       => throw new Exception("Cannot perform exponential on Character.")
   }
 }
 
 case class Float(value: Double) extends Numeric {
   override def +(that: Numeric): Numeric = that match {
-    case Integer(thatValue) => Float(value + thatValue)
-    case Float(thatValue)   => Float(value + thatValue)
+    case Integer(thatValue)   => Float(value + thatValue)
+    case Float(thatValue)     => Float(value + thatValue)
+    case Character(thatValue) => Float(value + thatValue)
   }
 
   override def -(that: Numeric): Numeric = that match {
-    case Integer(thatValue) => Float(value - thatValue)
-    case Float(thatValue)   => Float(value - thatValue)
+    case Integer(thatValue)   => Float(value - thatValue)
+    case Float(thatValue)     => Float(value - thatValue)
+    case Character(thatValue) => Float(value - thatValue)
   }
 
   override def *(that: Numeric): Numeric = that match {
-    case Integer(thatValue) => Float(value * thatValue)
-    case Float(thatValue)   => Float(value * thatValue)
+    case Integer(thatValue)   => Float(value * thatValue)
+    case Float(thatValue)     => Float(value * thatValue)
+    case Character(thatValue) => Float(value * thatValue)
   }
 
   override def /(that: Numeric): Numeric = that match {
-    case Integer(thatValue) => Float(value / thatValue)
-    case Float(thatValue)   => Float(value / thatValue)
+    case Integer(thatValue)   => Float(value / thatValue)
+    case Float(thatValue)     => Float(value / thatValue)
+    case Character(thatValue) => Float(value / thatValue)
   }
 
   override def %(that: Numeric): Numeric = that match {
-    case Integer(thatValue) => Float(value % thatValue)
-    case Float(thatValue)   => Float(value % thatValue)
+    case Integer(thatValue)   => Float(value % thatValue)
+    case Float(thatValue)     => Float(value % thatValue)
+    case Character(thatValue) => Float(value % thatValue)
   }
 
   override def ^(that: Numeric): Numeric = that match {
     case Integer(thatValue) => Float(Math.pow(value, thatValue))
     case Float(thatValue)   => Float(Math.pow(value, thatValue))
+    case Character(_)       => throw new Exception("Cannot perform exponential on Character.")
   }
+}
+
+case class Character(value: Char) extends Numeric {
+  override def +(that: Numeric): Numeric = that match {
+    case Integer(thatValue)   => Integer(value + thatValue)
+    case Float(thatValue)     => Float(value + thatValue)
+    case Character(thatValue) => Character((value + thatValue).toChar)
+  }
+
+  override def -(that: Numeric): Numeric = that match {
+    case Integer(thatValue)   => Integer(value - thatValue)
+    case Float(thatValue)     => Float(value - thatValue)
+    case Character(thatValue) => Character((value - thatValue).toChar)
+  }
+
+  override def *(that: Numeric): Numeric = that match {
+    case Integer(thatValue)   => Integer(value * thatValue)
+    case Float(thatValue)     => Float(value * thatValue)
+    case Character(thatValue) => Character((value * thatValue).toChar)
+  }
+
+  override def /(that: Numeric): Numeric = that match {
+    case Integer(thatValue)   => Integer(value / thatValue)
+    case Float(thatValue)     => Float(value / thatValue)
+    case Character(thatValue) => Character((value / thatValue).toChar)
+  }
+
+  override def %(that: Numeric): Numeric = that match {
+    case Integer(thatValue)   => Integer(value % thatValue)
+    case Float(thatValue)     => Float(value % thatValue)
+    case Character(thatValue) => Character((value % thatValue).toChar)
+  }
+
+  override def ^(that: Numeric): Numeric = throw new Exception("Cannot perform exponential on Character.")
 }
