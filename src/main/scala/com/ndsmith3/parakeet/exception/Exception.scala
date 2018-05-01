362b146715f9772faea1cdf055318cccd2741476
Nick Smith <nsmith458914@gmail.com>
2018-05-01 14:28:58 -0400

    Add CharacterPowerException.

--- src/main/scala/com/ndsmith3/parakeet/exception/Exception.scala
+++ src/main/scala/com/ndsmith3/parakeet/exception/Exception.scala
@@ -1,7 +1,7 @@
 package com.ndsmith3.parakeet.exception
 
 import com.ndsmith3.parakeet.lexer.Token
-
 class NoClosingParenthesisException()          extends Exception("Expected Closing Parenthesis.")
-class UnexpectedTokenException(token: Token)   extends Exception(s"Unexpected Token: $token")
-class UnexpectedCharacterException(char: Char) extends Exception(s"Unexpected Character: $char")
+class UnexpectedTokenException(token: Token)   extends Exception(s"Unexpected Token: $token.")
+class UnexpectedCharacterException(char: Char) extends Exception(s"Unexpected Character: $char.")
+class CharacterPowerException                  extends Exception(s"Cannot perform exponential operation on Character.")
